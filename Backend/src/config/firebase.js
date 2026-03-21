00bf063135d00b4a94214fd0e16ec133e5f91964
Daniel Arce <daniel.arce.campos@gmail.com>
2026-03-20 22:15:03 -0600

    Add firebase admin sdk

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -1,1 +1,13 @@
 // Inicializa firebase admin
+
+const admin = require('firebase-admin');
+const serviceAccount = require('../../serviceAccountKey.json');
+
+// Solo inicializa una vez aunque el archivo se importe varias veces
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+module.exports = admin;
